6c472ac73d5eb172f7adf3b2495b67f93da26a79
leeseolin <lsl9787@naver.com>
2024-04-14 21:20:38 +0900

    Updates

---
+++ precache-manifest.e22f52bd19cbb2031c5a255bfdcb3fc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a40a8b22fbcf17c0eaa317e8145df016",
+    "url": "/2024-portfolio/index.html"
+  },
+  {
+    "revision": "f9408504ddaa92b03c55",
+    "url": "/2024-portfolio/static/css/main.e1a96d98.chunk.css"
+  },
+  {
+    "revision": "8fb061d189700098954e",
+    "url": "/2024-portfolio/static/js/2.7fe5ee89.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2024-portfolio/static/js/2.7fe5ee89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9408504ddaa92b03c55",
+    "url": "/2024-portfolio/static/js/main.ee62561b.chunk.js"
+  },
+  {
+    "revision": "e5271af2a561d1b22a0a",
+    "url": "/2024-portfolio/static/js/runtime-main.f28b82f8.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/2024-portfolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "0ff43718544cfaa0f943cf4948cef421",
+    "url": "/2024-portfolio/static/media/your photo.0ff43718.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/2024-portfolio/precache-manifest.e22f52bd19cbb2031c5a255bfdcb3fc3.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/2024-portfolio/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
